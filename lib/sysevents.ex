17e535d3a9eba2ec2637d06e1fd864a99b9c79b6
gbigwood <greg.bigwood@gmail.com>
2017-06-17 13:14:17 +0100

    chore(build): add in the basic project shape

---
+++ lib/sysevents.ex
@@ -0,0 +1,18 @@
+defmodule Sysevents do
+  @moduledoc """
+  Documentation for Sysevents.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sysevents.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sysevents_test.exs
@@ -0,0 +1,8 @@
+defmodule SyseventsTest do
+  use ExUnit.Case
+  doctest Sysevents
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
