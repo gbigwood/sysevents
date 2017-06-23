b4595b164b1688bc6253bd90f12b566da54ade4e
gbigwood <gregory.bigwood@springer.com>
2017-06-23 11:46:07 +0100

    feature(tests): add put test

--- test/sysevents_test.exs
+++ test/sysevents_test.exs
@@ -1,11 +1,13 @@
 defmodule SyseventsTest do
   use ExUnit.Case, async: true
   use Plug.Test
+  require Sysevents.Event
+  alias Sysevents.Event, as: Event
   doctest Sysevents
 
   @opts Sysevents.init([])
 
-  test "returns repsonse with id" do
+  test "returns reponse with id" do
     # Create a test connection
     conn = conn(:get, "/chain")
 
@@ -16,5 +18,19 @@ defmodule SyseventsTest do
     assert conn.state == :sent
     assert conn.status == 200
     assert Poison.decode!(conn.resp_body)["id"] == "0123"
+  end
+
+
+  test "accepts put" do
+    # Create a test connection
+    conn = conn(:put, "/chain/123", Poison.encode!(%Event{id: "0123"}))
+	   |> put_req_header("content-type", "application/json")
+
+    # Invoke the plug
+    conn = Sysevents.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
   end
 end
