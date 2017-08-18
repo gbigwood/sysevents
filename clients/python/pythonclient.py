4b2a9ed8257c1c143fc0fe6f86360be7dde1226d
gbigwood <gregory.bigwood@springer.com>
2017-08-18 11:27:27 +0100

    feature(pythonclient): tidy example

--- clients/python/pythonclient.py
+++ clients/python/pythonclient.py
@@ -1,6 +1,12 @@
-import inspect
 import threading
+import requests
 from uuid import uuid4
+
+
+# TODO extensions:
+# * Why not just use a global stack? -- because we want thread specific tracing
+# * How to inject stack across thread boundaries? -- e.g. spawner of thread pool
+# * Interplay with co-routines?
 
 
 THREAD_LOCAL = threading.local()  # 🙈 Each call gets a view so must be global or passed in 🙈 
@@ -15,13 +21,14 @@ def _find_parent_id(thread_locals):
 
 def _save_current_frame(thread_locals):
     curr_id = str(uuid4())
-    # can't add attributes to frames cause they are wrappers for C (interpreter implementation specific)
+    # Can't add attributes to frames cause they are wrappers for C struct (interpreter
+    # implementation specific)
     thread_locals.frame_stack.append(curr_id)
-    print("pushed", curr_id)
     return curr_id
 
 
 def _construct_frame_stack(thread_locals):
+    # You're not supposed to recurse in python so stack per tracked call should be fine :)
     frame_stack = getattr(thread_locals, 'frame_stack', None)
     if not frame_stack:
         thread_locals.frame_stack = []
@@ -31,48 +38,59 @@ def _pop_stack(thread_locals):
     thread_locals.frame_stack.pop()
 
 
-def _save_on_server(parent_id, current_id):
-    print("saved", parent_id, current_id)
+def _save_on_server(parent_id, current_id, wrapped_func, info):
+    print("saved {!s:36} {:36} {:36} {!s}".format(parent_id, current_id, wrapped_func.__name__, info))
 
 
-def label_and_find_in_stack(func):
-    # You're not supposed to recurse in python so it's all good :)
-    def func_wrapper():
-        _construct_frame_stack(THREAD_LOCAL)
-        parent_id = _find_parent_id(THREAD_LOCAL)
-        current_id = _save_current_frame(THREAD_LOCAL)
-        _save_on_server(parent_id, current_id)
-        result = func()
-        _pop_stack(THREAD_LOCAL)
-        return result
-    return func_wrapper
+def trace(info):
+    def decorator(func):
+        def func_wrapper(*args, **kwargs):
+            _construct_frame_stack(THREAD_LOCAL)
+            parent_id = _find_parent_id(THREAD_LOCAL)
+            current_id = _save_current_frame(THREAD_LOCAL)
+            _save_on_server(parent_id, current_id, func, info)
+            result = func(*args, **kwargs)
+            _pop_stack(THREAD_LOCAL)
+            return result
+        return func_wrapper
+    return decorator
 
 
 ############################
 
 
-@label_and_find_in_stack
-def other_labelled_function():
+@trace("specific sub system")
+def remove_expired_pipelines():
     pass
 
 
-@label_and_find_in_stack
-def some_other_branch():
+@trace("remove invalid branches")
+def remove_invalid_branches():
     pass
 
 
-def function_that_does_something_else():
-    other_labelled_function()
-    some_other_branch()
+@trace("get all repos")
+def get_all_repos():
+    remove_invalid_branches()
 
 
-@label_and_find_in_stack
-def function_that_does_thing():
-    function_that_does_something_else()
+@trace("get all pipelines")
+def get_all_pipelines():
+    remove_expired_pipelines()
+
+
+def untraced_intermediate_layer():
+    get_all_repos()
+    get_all_pipelines()
+
+
+@trace("inception")
+def spawn_inception_handler():
+    untraced_intermediate_layer()
 
 
 def main():
-    function_that_does_thing()
+    spawn_inception_handler()
 
 
 if __name__ == "__main__":
--- clients/python/test_pythonclient.py
+++ clients/python/test_pythonclient.py
@@ -1,5 +1,8 @@
 from pythonclient import function_that_does_thing, thread_local
 
 def test_wrapper():
+    # TODO create a stub for the side effect catcher
+    # make the trace decorator have a default argument for _save_on_server
+    # override save on server using stub for testing
 
     pass
